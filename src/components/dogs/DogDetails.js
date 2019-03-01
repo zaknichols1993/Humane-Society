51cf0106ea1e5221e32082279b019aac9f8b4a74
zaknichols1993 <zaknichols1993@gmail.com>
2019-03-01 01:32:36 -0500

    Added SignIn component. Changed a few things in DogDetails component.

--- src/components/dogs/DogDetails.js
+++ src/components/dogs/DogDetails.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-const DogDetails = () => {
-  return (
-    <div className="card z-depth-0 project-summary">
-    <div className="card-content grey-text text-darken-3">
-      <span className="card-title ">Dog Name</span>
-      <p>lorem asdiofjasdofijasdfkoj askdfj ldksadlf jwkg jwqoghqrhigqrg</p>
-      <p>Posted by Jill Richardson</p>
-      <p className="grey-text">March 1st</p>
-    </div>
-  </div>
-  )
+const DogDetails = (props) => {
+    const id = props.match.params.id;
+    return (
+        <div className="container section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title ">Dog Name {id}</span>
+                    <p>lorem asdiofjasdofijasdfkoj askdfj ldksadlf jwkg jwqoghqrhigqrg</p>
+                </div>
+                <div className="card-action lighten-4 grey-text">
+                    <p>Posted by Jill Richardson</p>
+                    <p className="grey-text">March 1st</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default DogDetails;
